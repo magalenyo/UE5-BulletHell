2a381860ead683071327f8e89aa10432550aad35
magalenyo <magalenyo7@gmail.com>
2023-08-11 00:40:16 +0200

    Modify projectile material

--- Source/BulletHell/Attacks/Attack_Ixion_HAHomingAndLaser.h
+++ Source/BulletHell/Attacks/Attack_Ixion_HAHomingAndLaser.h
@@ -48,7 +48,7 @@ private:
 	float bulletsSpeed = 1400.0f;
 
 	UPROPERTY(EditAnywhere, Category = "Heavy Attack: HomingAndLaser - Bullets")
-	float bulletsHomingMagnitude = 2200.0f;
+	float bulletsHomingMagnitude = 3000.0f;
 
 	UPROPERTY(EditAnywhere, Category = "Heavy Attack: HomingAndLaser - Bullets")
 	float bulletsMinTimeWave = .03f;
